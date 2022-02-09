bc6d5e0dce79637a966195635b294e7e6a50fa72
weronikadominiak <weronika.dominiak@hotmail.com>
2022-02-09 21:08:59 +0000

    feature: switch tabs during test

--- client/test/specs/example.e2e.js
+++ client/test/specs/example.e2e.js
@@ -1,11 +1,14 @@
 describe("My first test", () => {
   it("should login with valid credentials", async () => {
-    await browser.url("/admin");
+    // admin
+    await browser.url("http://localhost:3000/admin");
     await browser.saveScreenshot("./here.png");
     await $("button").click();
     const id = await $("#roomId").getText();
     await browser.saveScreenshot("./here2.png");
-    await browser.url("/");
+
+    // get into game
+    await browser.newWindow("/");
     await browser.saveScreenshot("./here3.png");
     const name = $("#name");
     await name.setValue("Nika");
@@ -14,5 +17,11 @@ describe("My first test", () => {
     await browser.saveScreenshot("./here4.png");
     await $("button").click();
     await browser.saveScreenshot("./here5.png");
+    await browser.pause(2000);
+
+    // back to admin
+    await browser.switchWindow("http://localhost:3000/admin");
+    await browser.pause(2000);
+    await browser.saveScreenshot("./here6.png");
   });
 });
