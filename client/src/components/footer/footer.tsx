582a2dc9a7e0679b81b7a5b87d9cf1bd96039238
weronikadominiak <weronika.dominiak@hotmail.com>
2022-03-17 23:46:46 +0000

    feat: footer

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -1,5 +1,31 @@
+import styles from "./footer.module.scss";
+
 function Footer() {
-  return <footer>Footer @ 2020</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.col}>
+        <a
+          href="https://drive.google.com/drive/folders/1pGIcpZVsK-g0MEEoQO-icj5EVoQBByK8?usp=sharing"
+          target="_blank"
+        >
+          Facilitation Guide
+        </a>
+        <a
+          href="https://www.paypal.com/donate/?hosted_button_id=XKHQ46HFVQ7LE"
+          target="_blank"
+        >
+          Donate
+        </a>
+      </div>
+
+      <div className={styles.contact}>
+        Contact us:{" "}
+        <a href=" flippinsupport@flippingames.com">
+          flippinsupport@flippingames.com
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
