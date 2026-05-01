a5553ee7c4860c9f40a45b8480c37f435b6fa8a8
David Spinks <53400942+sticksandbones@users.noreply.github.com>
2026-05-01 16:44:53 +0200

    Added logging in app.ts to see if Express is being reached

--- socketio-server/src/app.ts
+++ socketio-server/src/app.ts
@@ -11,6 +11,11 @@ var indexRouter = require("./routes/index");
 var adminRouter = require("./routes/admin");
 
 var app = express();
+app.use((req, res, next) => {
+  console.log("REQUEST:", req.method, req.url);
+  next();
+});
+
 require("dotenv").config();
 
 // view engine setup
